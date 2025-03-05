b9a0a293edceed782072c196d8a8511f1036c2c9
QHarris11 <197433186+QHarris11@users.noreply.github.com>
2025-03-05 13:53:23 -0500

    Added dictionaries file

---
+++ Swift_Practice/Dictionaries.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+//Dictionaries
+let heights = [
+    "Johnny Blaze": 1.78,
+    "Ed Sheeran": 1.73
+    ]
+heights["Johnny Blaze"]
+print(heights)
+//Dictionaries are collections of values just like arrays, but rather than storing things with an integer position you can access them using anything you want. When using type annotations, dictionaries are written in brackets with a colon between your identifier and valu types. Ex: [Sting: Double] and [String: String]
