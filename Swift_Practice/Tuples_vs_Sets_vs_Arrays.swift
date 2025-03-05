acd41a4e599d6a34f46bccbbaddd783ddc653906
QHarris11 <197433186+QHarris11@users.noreply.github.com>
2025-03-05 13:56:29 -0500

    Updated Tuples_vs_Sets_vs_Arrays

--- Swift_Practice/Tuples_vs_Sets_vs_Arrays.swift
+++ Swift_Practice/Tuples_vs_Sets_vs_Arrays.swift
@@ -1,10 +1,13 @@
 import UIKit
 
 //tuples vs sets vs arrays
+//tuple
 let address = (house: 555, street: "Taylor Swift Ave", city: "Nashville")
 print(address)
+//set
 let set = Set(["aardvark", "astronaut", "azalea"])
 print(set)
+//array
 let pythons = ["Eric", "Graham", "John", "Michael", "Terry", "Terry"]
 print(pythons)
 // If you need a specific, fixed collection of related values where each item has a precise position or name, use tuple. if you need a collection of values that must be unique or you need to be able to check whether a specific item is in there extremely quickly, you should use a set. if you need a collection of values that can contain duplicates, or the order of your items matters, use an array. arrays are most common
